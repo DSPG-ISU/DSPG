9e317f2b7154b74595e5cf36ca6f261ac9bb61f5
heike <hofmann@iastate.edu>
2020-07-06 15:34:00 -0500

    change Location to sf object

--- stuff/data-upload-heike.R
+++ stuff/data-upload-heike.R
@@ -3,10 +3,29 @@ path_to_box <- "~/Box/DSPG Shared Data/Syscare_Scraped_Data/"
 files <- dir(path_to_box, pattern = "csv", recursive = TRUE,
              full.names = TRUE)
 
+text_to_geometry <- function(x) {
+  # input: vector of characters of the form POINT (-93.5878033 41.5620619)
+  # output: data frame of the same length as input with columns Longitude and Latitude
+
+  # split along the white space
+  listx <- strsplit(x, split=" ")
+  arrayx <- matrix(unlist(listx), ncol = 3, byrow=TRUE)
+
+  data.frame(
+    Longitude = parse_number(arrayx[,2]),
+    Latitude = parse_number(arrayx[,3])
+  )
+}
+
 acs <- readr::read_csv(files[1])
-usethis::use_data(acs)
 
+# add two variables: Latitude and Longitude
+acs <- data.frame(acs, text_to_geometry(acs$Location))
+# convert Longitude and Latitude into point object
+acs <- acs %>% sf::st_as_sf(coords = c("Longitude", "Latitude"),
+                            crs = 4326, agr = "identity")
+# remove Location variable
+acs <- acs %>% select(-Location)
 
+usethis::use_data(acs, overwrite = TRUE)
 
-names(colleges)[8:9] <- c("Longitude", "Latitude")
-usethis::use_data(colleges, overwrite = TRUE)
