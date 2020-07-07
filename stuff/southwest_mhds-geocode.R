d94673a8aea6eb87eca0cd40982bab0de808e2c0
Andrew Maloney <amaloney@iastate.edu>
2020-07-07 13:02:48 -0400

    Added southwest_mhds geocode script

---
+++ stuff/southwest_mhds-geocode.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(ggmap)
+library(ggplot2)
+library(dplyr)
+library(sf)
+library(leaflet)
+southwest_mhds <- readr::read_csv("swiamhds.csv")
+
+#cf_resources <- cf_resources %>% mutate(
+  #search_address = paste(Address, City, State, Zip, sep = ", ")
+#)
+
+register_google(key = "your api key", write = TRUE)
+
+southwest_mhds <- southwest_mhds %>%
+  filter(!is.na(Address)) %>%
+    mutate_geocode(Address)  #One NA value due to Address
+
+# don't include the variable for the ID
+southwest_mhds <- southwest_mhds %>% select(-X1)
+
+usethis::use_data(southwest_mhds, overwrite = TRUE)
