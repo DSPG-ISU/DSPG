2cb9f1e0a032874e6dc1cb7fe36b6a88728da324
mavoss16 <mavoss16@gmail.com>
2020-07-07 17:11:12 -0500

    Added hospital buildings dataset

---
+++ stuff/hospital-buildings.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+file = "~/Box Sync/DSPG Shared Data/Syscare_Scraped_Data/Iowa-Hospital-Buildings.csv"
+
+text_to_geometry <- function(x) {
+  # input: vector of characters of the form POINT (-93.5878033 41.5620619)
+  # output: data frame of the same length as input with columns Longitude and Latitude
+
+  # split along the white space
+  listx <- strsplit(x, split=" ")
+  arrayx <- matrix(unlist(listx), ncol = 3, byrow=TRUE)
+
+  data.frame(
+    Longitude = parse_number(arrayx[,2]),
+    Latitude = parse_number(arrayx[,3])
+  )
+}
+
+hospital_buildings <- readr::read_csv(file)
+
+# add two variables: Latitude and Longitude
+hospital_buildings <- data.frame(hospital_buildings, text_to_geometry(hospital_buildings$`Primary Point (Coordinates)`))
+# convert Longitude and Latitude into point object
+hospital_buildings <- hospital_buildings %>% sf::st_as_sf(coords = c("Longitude", "Latitude"),
+                            crs = 4326, agr = "identity")
+# remove Primary Point Coordinates variable
+hospital_buildings <- hospital_buildings %>% select(-Primary.Point..Coordinates.)
+
+usethis::use_data(hospital_buildings, overwrite = TRUE)
+
