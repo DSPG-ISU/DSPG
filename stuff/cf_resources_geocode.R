852f0562ceec421ce8af3c4921459d348c2c56c0
Andrew Maloney <amaloney@iastate.edu>
2020-07-07 00:28:10 -0400

    Geocode for Community & Family Resources data

---
+++ stuff/cf_resources_geocode.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(ggmap)
+library(ggplot2)
+library(dplyr)
+library(sf)
+library(leaflet)
+cf_resources <- readr::read_csv("cfrhelp_scrape.csv")
+
+cf_resources <- cf_resources %>% mutate(
+  search_address = paste(Address, City, State, Zip, sep = ", ")
+)
+
+register_google(key = "AIzaSyCMpgQfz4_ugBl68tfeXu0ZHnskt_WpLeM", write = TRUE)
+cf_resources <- cf_resources %>% mutate_geocode(search_address)  #One NA value due to Address
+
+# don't include the variable for the ID
+cf_resources <- cf_resources %>% select(-X1)
+
+usethis::use_data(cf_resources, overwrite = TRUE)
+
+
