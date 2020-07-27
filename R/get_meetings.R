3bc719f17de9c4196b5c64498447bc04c9d0e55b
heike <hofmann@iastate.edu>
2020-07-26 22:32:54 -0500

    getting schedule

--- R/get_meetings.R
+++ R/get_meetings.R
@@ -5,7 +5,7 @@
 #' for a specified time range.
 #' @param from earliest date (and time, if specified)
 #' @param to latest date
-#' @param type type of meeting
+#' @param type type of meeting see `unique(meetings$type)`
 #' @return data frame
 #' @export
 #' @importFrom lubridate now days wday weeks ymd_hms
@@ -20,20 +20,20 @@
 #' # color locations by day
 #' pal <- colorFactor(
 #'          palette = RColorBrewer::brewer.pal(n=7, name="Paired"),
-#'          domain = levels(meetings$Day)
+#'          domain = levels(meetings$day)
 #'        )
 #' get_meetings() %>%
 #'   leaflet() %>%
 #'   addTiles() %>%
 #'   addPolygons(data = st_transform(ia_counties, crs='+proj=longlat +datum=WGS84'),
 #'               weight = 1, color="#888888") %>%
-#'   addCircleMarkers(lng = ~lon, lat = ~lat, color=~pal(Day),
-#'                    label=~Meeting,
+#'   addCircleMarkers(lng = ~longitude, lat = ~latitude,
+#'                    color=~pal(day), label=~meeting,
 #'                    radius = 1, stroke = 0.1) %>%
-#'   addLegend(pal = pal, values = levels(meetings$Day))
+#'   addLegend(pal = pal, values = levels(meetings$day))
 get_meetings <- function(from = now(), to = now() + days(1), type = c("Alcoholics Anonymous", "Narcotics Anonymous")) {
   timestamp <- NULL  # just to pass R CMD CHECK
-  Type <- NULL
+  type <- NULL
 
   # which weekday is the earliest date?
   days_since_monday <- 1 - wday(from, week_start = 1)
@@ -52,5 +52,5 @@ get_meetings <- function(from = now(), to = now() + days(1), type = c("Alcoholic
   # reduce schedule to date range specified and type of meeting
   filter(schedule, between(ymd_hms(timestamp),
                            ymd_hms(from), ymd_hms(to)),
-         Type %in% type)
+         type %in% type)
 }
