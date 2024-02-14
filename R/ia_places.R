3cdec7b4622f21e5ec87f2e66219f6a17de1ecac
Heike Hofmann <hofmann@iastate.edu>
2024-02-14 10:17:30 -0600

    added population numbers

--- R/ia_places.R
+++ R/ia_places.R
@@ -1,13 +1,30 @@
 #' Shapefiles of places in IA in 2020
 #'
-#'
-#' @format A tibble with 1028 rows and 5 columns
+#' The shapefiles are based on places in Iowa in 2020. The names of places and its population numbers are based on
+#' the decennial census counts for 2020, 2010, and 2000.
+#' Note that the US Census Bureau used differential privacy estimates for the 2020 Census, which might impact the
+#' exact numbers below county levels. In particular, for small areas, these estimates might differ from the actual
+#' population numbers.
+#' @format A tibble with 1030 rows and 7 columns
 #' \describe{
-#'   \item{GEOID}{character}
-#'   \item{NAME}{character}
-#'   \item{variable}{character}
-#'   \item{value}{numeric}
-#'   \item{geometry}{sfc_MULTIPOLYGON}
+#'   \item{geoid}{character}
+#'   \item{name}{character}
+#'   \item{geometry}{sfc_MULTIPOLYGON, based on the PL 94-171 Redistricting Data Summary File}
+#'   \item{pop2020}{numeric, 2020 Census Variable P1_001N}
+#'   \item{pop2010}{numeric, 2010 Census Variable P001001}
+#'   \item{pop2000}{numeric, 2000 Census Variable PL001001}
 #' }
-#' @source US Census Bureau 2020
+#' @source US Census Bureau Decennial Census 2020, 2010, 2000
+#' @examples
+#' # three geometries on top of each other
+#' library(leaflet)
+#' library(sf)
+#' ia_cities %>%
+#'   leaflet() %>%
+#'   addTiles() %>%
+#'   addPolygons(data = ia_counties,
+#'              weight = 1, color="#333333") %>%
+#'   addPolygons(data = ia_places, weight = 1, color="maroon") %>%
+#'   addCircleMarkers(radius = 0,
+#'                    label = ~city)
 "ia_places"
--- stuff/geographic-shapefiles.R
+++ stuff/geographic-shapefiles.R
@@ -47,6 +47,52 @@ names(ia_places) <- tolower(names(ia_places))
 ia_places <- ia_places %>% mutate(
   name = gsub(" city, Iowa", "", name)
 )
+ia_places <- ia_places %>% mutate(
+  geoid = as.numeric(geoid),
+  pop20 = value
+) %>% select(-variable, -value)
+
+
+pop10 <- get_decennial(
+  geography = "place",
+  variables = "P001001",
+  state="Iowa",
+  year = 2010
+)
+names(pop10) <- tolower(names(pop10))
+pop10 <- pop10 %>% mutate(
+  geoid = as.numeric(geoid),
+  pop10 = value,
+) %>% select(-variable, -value)
+
+
+pop00 <- get_decennial(
+  geography = "place",
+  variables = "PL001001",
+  state="Iowa",
+  year = 2000,
+  sumfile = "pl"
+)
+names(pop00) <- tolower(names(pop00))
+pop00 <- pop00 %>% mutate(
+  geoid = as.numeric(geoid),
+  pop00 = value,
+) %>% select(-variable, -value)
+
+
+ia_places <- ia_places %>%
+  full_join(pop10 %>% select(-name), by="geoid") %>%
+  full_join(pop00 %>% select(-name), by="geoid")
+
+
+names(ia_places)[5:7] <- c("pop2020", "pop2010", "pop2000")
+
+# for which geoids do we not have names?
+ids <- ia_places %>% filter(is.na(name)) %>% as_tibble() %>% select(geoid) %>% as.vector()
+filter(pop00, geoid %in% ids$geoid)
+
+ia_places$name[ia_places$geoid==1945750] <- "Littleport"
+ia_places$name[ia_places$geoid==1952410] <- "Millville"
 
 usethis::use_data(ia_places, overwrite = TRUE)
 
